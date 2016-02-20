723aa48b21259cfe6504ec01e6e98137c9d5fc8d
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2016-02-20 13:13:50 -0500

    Revert "Added basic expand and collapse functions to cards with related icons. No live filtering yet."
    This reverts commit 1e03c5b56b07b607a3444e7d4a87eead5443e96b.

--- js/meetup.js
+++ js/meetup.js
@@ -21,7 +21,7 @@ var Meetup = function(meetupURL) {
 	var requested_page = window.location.search.match(/page=(\d+)/);
 	var offset = (requested_page == null ? 0 : parseInt(requested_page[1]) - 1);
 	offset = (offset < 0 ? 0 : offset);
-	this.developersURL = "https://api.meetup.com/2/members?key=6e7b85c3fc96f524d133273a4556&group_urlname=newhavenio&sign=true&photo-host=public&order=name&page=100" +
+	this.developersURL = "https://api.meetup.com/2/members?key=6e7b85c3fc96f524d133273a4556&group_urlname=newhavenio&sign=true&photo-host=public&order=name&page=40" +
 	                     "&offset=" + offset;
 
 	this.getDevelopers = function(callback) {
@@ -37,20 +37,3 @@ var Meetup = function(meetupURL) {
 		});
 	};
 };
-
-$(document).on('click', '.developer-card .dev-card-trigger', function (event) {
-	$(this).closest('.developer-card').toggleClass('--small');
-
-	if ( $(this).hasClass('glyphicon-plus') ) {
-		$(this).removeClass('glyphicon-plus').addClass('glyphicon-minus');
-	} else {
-		$(this).removeClass('glyphicon-minus').addClass('glyphicon-plus');
-	}
-});
-
-
-
-
-
-
-
