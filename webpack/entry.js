4e515bc015bb19f62c8317a3d34126ae2bcc6986
Brady <bsutt123@gmail.com>
2018-05-08 19:49:44 -0400

    add function to load vue component to the dom

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,6 +1,17 @@
-window.$ = require('jquery');
-window.Mustache = require('mustache');
+window.$ = require("jquery");
+window.Mustache = require("mustache");
+import Vue from "vue";
+import "bootstrap-sass/assets/javascripts/bootstrap.js";
 
-import 'bootstrap-sass/assets/javascripts/bootstrap.js';
+import "./scss_files/main.scss";
 
-import './scss_files/main.scss';
+import MeetupEvents from "../js/vue/meetupEvents.vue";
+
+document.addEventListener("DOMContentLoaded", function() {
+  var eventsApp = new Vue({
+    el: "#meetup-events",
+    render: function(h) {
+      return h(MeetupEvents);
+    }
+  });
+});
