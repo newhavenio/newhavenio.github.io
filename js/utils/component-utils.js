e356ddd79fb5e19e9af3d39d1b5b68cef6752e07
Brady <bsutt123@gmail.com>
2018-05-09 13:22:58 -0400

    include format date function

--- js/utils/component-utils.js
+++ js/utils/component-utils.js
@@ -1,3 +1,27 @@
+const DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
+];
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
 function parameterize(params) {
   return Object.keys(params)
     .map(function(key) {
@@ -6,6 +30,26 @@ function parameterize(params) {
     .join("&");
 }
 
+function formatDate(local_date, local_time) {
+  var dateArray = local_date.split("-");
+  var timeArray = local_time.split(":");
+
+  var date = new Date(
+    dateArray[0],
+    dateArray[1] - 1,
+    dateArray[2],
+    timeArray[0],
+    timeArray[1]
+  );
+
+  var ampm = timeArray[0] >= 12 ? "PM" : "AM";
+
+  return `${DAYS[date.getDay()]}, ${
+    MONTHS[date.getMonth()]
+  } ${date.getDate()} at ${timeArray[0] % 12}:${timeArray[1]} ${ampm}`;
+}
+
 module.exports = {
-  parameterize
+  parameterize,
+  formatDate
 };
