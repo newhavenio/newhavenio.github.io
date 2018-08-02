228e4316346e3e1ac9e29d7f5b54e84f5e957f74
Brady Sutton <bsutt123@gmail.com>
2018-08-01 21:07:38 -0400

    update webpack to use webpack 4 and modularized code

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,63 @@
 const path = require('path');
-const glob = require('glob')
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-const extractSass = new ExtractTextPlugin({
+const modeConfig = env => {
+  return require(`./build-utils/webpack.${env}.js`)(env)
+}
+const presetConfig = require('./build-utils/loadPresets')
+
+const extractSass = new MiniCssExtractPlugin({
   filename: "../_includes/[name].css"
 });
 
-const uglifyJs = new UglifyJsPlugin({
-});
-
-module.exports = {
-  entry: "./webpack/entry.js",
-  output: {
-    path: path.resolve(__dirname, "js/"),
-    filename: "bundle.js"
-  },
-  module: {
-    loaders:[
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        loader: "babel-loader",
-        query: {
-          presets: ["react", "es2015"]
-        }
-      }
-    ],
-    rules: [
-      {
-        test: /\.(ttf|eot|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "file-loader"
-        }
+module.exports = ({
+  mode,
+  presets
+} = {
+  mode: "production",
+  presets: []
+}) => {
+  return webpackMerge({
+      mode,
+      entry: "./webpack/entry.js",
+      output: {
+        path: path.resolve(__dirname, "js/"),
+        filename: "bundle.js"
       },
-      {
-        test: /\.scss$/,
-        use: extractSass.extract({
-          use: [
-            {
-              loader: "css-loader",
-              options: {
-                minimize: true,
-                importLoaders: 1
-              }
-            },
-            {
-              loader: "postcss-loader"
-            },
-            {
-              loader: "sass-loader"
+      module: {
+        rules: [{
+            test: /\.(ttf|eot|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "file-loader"
             }
-          ],
-          fallback: "style-loader"
-        })
-      }
-    ],
-  },
-  plugins:[
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    extractSass,
-    uglifyJs
-  ]
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "postcss-loader",
+              "sass-loader"
+            ]
+          }
+        ],
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery'
+        }),
+        extractSass,
+        new webpack.ProgressPlugin()
+      ]
+    },
+    modeConfig(mode),
+    presetConfig({
+      mode,
+      presets
+    })
+  )
 }
